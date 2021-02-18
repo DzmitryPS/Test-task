f4e743e975adf038565cbfc887a89e78033517bf
dima <galtd@list.ru>
2021-02-18 19:20:01 +0300

    task_3

--- task_3/script.js
+++ task_3/script.js
@@ -1,37 +1,61 @@
 const Btn = document.getElementById('btn');
 const Field = document.getElementById('number');
-
-
 let searchUrl = window.location.search;
-
-
 let timer;
-function counter(n){
+let n;
 
-    n= n-1;
-    alert(n)
-    if(n<=0){
-        clearTimeout(timer);
-    } else {
-        timer = setTimeout(counter(n), 1000);
-      }
+function counter(n) {
+  Btn.innerHTML = n + ' sec';
+  Field.value = n;
+  n = n - 1;
+  if (n < 0) {
+    Field.disabled = 0;
+    Btn.innerHTML = 'button';
+    clearTimeout(timer);
+  } else {
+    Field.disabled = 1;
+    timer = setTimeout(() => counter(n), 1000);
+  }
 }
 
+//        |\   /|
+//         \|_|/
+//         /. .\
+//        =\_Y_/=
+//         {>o<}
 
 
-document.addEventListener("DOMContentLoaded",function (event) {
-    let userInputCheck = searchUrl.split('=');
-     let n = parseInt(userInputCheck[1])
-    if(searchUrl.includes('sec')
-    && userInputCheck[1]
-    && userInputCheck[1] > 0
-    && Number.isInteger(n)
-    ){
-     Btn.innerHTML = n + ' sec'
-     Field.value = n;
-
-    counter(n);
+document.addEventListener('DOMContentLoaded', function (event) {
+  let userInputCheck = searchUrl.split('=');
+  n = parseInt(userInputCheck[1]);
+  Btn.addEventListener('click', function (event) {
+    if (Field.disabled === false && Field.value != '0') {
+      event.preventDefault();
+      n = parseInt(Field.value);
+      window.location.replace(window.location.pathname + '?sec='+ n + window.location.hash)
     }
-
-    })
-
+    else if (n > 0 && Field.value != '0') {
+      event.preventDefault();
+      Field.disabled = 0;
+      clearTimeout(timer);
+      Btn.innerHTML = 0 + ' sec';
+      Field.value = 0;
+      Btn.innerHTML = 'button';
+    }
+    else {
+      event.preventDefault();
+      console.log('counter stoped')
+    }
+  })
+  if (
+    searchUrl.includes('sec') &&
+    userInputCheck[1] &&
+    userInputCheck[1] > 0 
+    && Number.isInteger(n)
+  ) {
+    counter(n);
+  } else {
+    n = 5;
+    counter(5);
+  }
+});
